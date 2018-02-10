f646b9577cbb49d7fc5cb55a4edcba4fff47b941
dex.papa@gmail.com <dex.papa@gmail.com>
2018-02-10 21:40:04 +0530

    Experiments with Deep Link

--- app/src/main/java/news/karanke/hmp/hmpnews/MainActivity.java
+++ app/src/main/java/news/karanke/hmp/hmpnews/MainActivity.java
@@ -59,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
                 webView.setVisibility(View.VISIBLE);
                 swipeRefreshLayout.setRefreshing(false);
             }
-
     });
 
 
